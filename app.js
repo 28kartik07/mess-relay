30f830b00ca23c406e1b8478f4de4e3a062543d1
28kartik07 <kartikchandra2000@outlook.com>
2024-01-08 00:44:44 +0530

    started

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const app = express();
+
+app.set('view-engine','ejs');
+
+app.route("/")
+.get(function(req,res){
+    res.render("home.ejs");
+});
+
+
+app.listen("3000",function(req,res){
+    console.log("server started");
+});
