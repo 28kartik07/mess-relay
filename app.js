e0036878cb604225d9285d84699ee3f478032e00
im_ayushjha <ayushjha2805@gmail.com>
2024-01-13 01:25:45 +0530

    login created

--- app.js
+++ app.js
@@ -1,29 +1,53 @@
+require('dotenv').config();
 const express = require("express");
 const bodyparser = require("body-parser");
 const mongoose=require("mongoose");
+const session=require("express-session");
+const passport=require("passport");
+const passportLocalMongoose=require("passport-local-mongoose");
 
-mongoose.connect("mongodb+srv://ayush2022ca016:ayush2022ca016@cluster0.4vonwx6.mongodb.net/?retryWrites=true&w=majority").then(()=>console.log("mongo connected")).catch(err=>console.log(err));
 const app = express();
 
-const userschema=new mongoose.Schema({
-    email : String,
-    password : String,
-    role : String,
-    tcomplaint : Number
-});
-
-const usermodel=mongoose.model("messrecord",userschema);
 
 app.use(bodyparser.urlencoded({extended:true}));    
 app.set('view-engine','ejs');
 app.use(express.static('public'));
 
-const user = new usermodel({
-    email : "ayush@mail.com",
-    password : "12334",
-    role : "student",
-    tcomplaint : 12
+app.use(session({
+    secret : process.env.KEY,
+    resave : false,
+    saveUninitialized : false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb+srv://ayush2022ca016:ayush2022ca016@cluster0.4vonwx6.mongodb.net/?retryWrites=true&w=majority").then(()=>console.log("mongo connected")).catch(err=>console.log(err));
+
+const userschema=new mongoose.Schema({
+    username : String,
+    password : String,
+    registration : String,
+    name : String,
+    hostel : String,
+    gender : String
 });
+
+userschema.plugin(passportLocalMongoose);
+
+const usermodel=mongoose.model("messrecord",userschema);
+
+passport.use(usermodel.createStrategy());
+
+// passport.serializeUser(usermodel.serializeUser());
+// passport.deserializeUser(usermodel.deserializeUser());
+passport.serializeUser(function(usermodel, done) {
+    done(null, usermodel);
+  });
+  
+  passport.deserializeUser(function(usermodel, done) {
+    done(null, usermodel);
+  });
 
 // user.save();
 
@@ -37,10 +61,29 @@ app.route("/login")
     res.render("login.ejs");
 })
 .post(function(req,res){
-    const user=req.body.email;
-    const pass=req.body.password;
-    console.log(user,pass);
-    res.redirect('/login');
+    const user=new usermodel({
+        username : req.body.username,
+        password : req.body.password,
+        registration : req.body.registration,
+        name : req.body.name,
+        hostel : req.body.hostel,
+        gender : req.body.gender
+    });
+
+    req.login(user,function(err){
+        if(err)
+            console.log(err);
+        else{
+            passport.authenticate("local",function(err,user,info){
+                if(err)
+                    console.log(err);
+                if(!user)
+                    res.render("login");
+                else
+                    res.redirect("/profile");
+            })(req,res);
+        }
+    });
 });
 
 app.route("/signup")
@@ -48,13 +91,21 @@ app.route("/signup")
     res.render("signup.ejs");
 })
 .post(function(req,res){
-    const name=req.body.name;
-    const email=req.body.email;
-    const registration=req.body.registration;
-    const password=req.body.password;
-    const hostel=req.body.hostel;
-    console.log(name,email,registration,password,hostel);
-    res.redirect('/signup');
+    usermodel.register({username: req.body.username,
+        name:req.body.name,
+        registration:req.body.registration,
+        hostel:req.body.hostel,
+        gender:req.body.inlineRadioOptions},req.body.password,function(err,user){
+        if(err){
+            console.log(err);
+            res.redirect("/signup");
+        }
+        else{
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/login");
+            });
+        }
+    });
 });
 
 app.route("/profile")
