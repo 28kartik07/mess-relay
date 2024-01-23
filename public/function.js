14911a95e811ebcfd1ad1075d0b82ee2db36b699
im_ayushjha <ayushjha2805@gmail.com>
2024-01-23 19:19:47 +0530

    like button added

--- public/function.js
+++ public/function.js
@@ -1,16 +1,17 @@
-var i = 0;
-
 function like() {
-  var c = document.getElementById("val").textContent;
-  c++;
-  i++;
-  console.log(i);
+  // var c = document.getElementById("val").value;
+  // if (c === "0") {
+  // document.getElementById("val").value = "1";
+  // var form = document.getElementById("myform");
+  // form.submit();
+  // }
+  // c++;
+  // i++;
+  // console.log(i);
 }
 
-function dislike() {
-    
-}
-function submitForm() {
-  var form = document.getElementById("myForm");
-  form.submit();
-}
+function dislike() {}
+// function submitForm() {
+//   var form = document.getElementById("myForm");
+//   form.submit();
+// }
