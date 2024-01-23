99003b6da9bf6308cd83ff9b9e541d01cb1e6701
im_ayushjha <ayushjha2805@gmail.com>
2024-01-23 12:09:26 +0530

    like function added

--- public/function.js
+++ public/function.js
@@ -1,5 +1,15 @@
-function like() {}
-function dislike() {}
+var i = 0;
+
+function like() {
+  var c = document.getElementById("val").textContent;
+  c++;
+  i++;
+  console.log(i);
+}
+
+function dislike() {
+    
+}
 function submitForm() {
   var form = document.getElementById("myForm");
   form.submit();
